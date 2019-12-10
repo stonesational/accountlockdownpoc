975882393719dbd3535c8d05b222663db129063b
jason.stone <jason.stone@salesforce.com>
2019-12-09 19:59:46 -0800

    Customer Header

--- force-app/main/default/aura/lockdown/lockdownController.js
+++ force-app/main/default/aura/lockdown/lockdownController.js
@@ -1,58 +1,52 @@
 ({
-    handleShowBasicModal: function(component, evt, helper) {
-        var workspaceAPI = component.find("workspace");
-        var tabId = workspaceAPI.getEnclosingTabId();
+    handleShowCustomModal: function(component, evt, helper) {
+        if( ! component.get("v.isActive") ) return;
         
-        var pageReference = {
-            type: 'standard__objectPage',
-            attributes: {
-                objectApiName: 'Account',
-                actionName: 'home'
+        /*var pageReference = {    
+            "type": "standard__navItemPage",
+            "attributes": {
+                 "apiName": "Authenticator"    
+            },
+            "state": {
+                "customerId": component.get("v.recordId")
+            }
+        };*/
+        var pageReference = {    
+            "type": "standard__component",
+            "attributes": {
+                 "componentName": "c__UnauthenticatedCustomerView"    
+            },
+            "state": {
+                "c__customerId": component.get("v.recordId")
             }
         };
-                
-        /*
-        workspaceAPI.getAllTabInfo().then(function(response) {
-            console.log(response);
-       }) */
+        
 
-       /*
-       workspaceAPI.openTab({
-            //pageReference: pageReference,
-            url: '/lightning/o/Account',
-            focus: true
-        });
-        */
-        component.find('overlayLib').showCustomModal({
-            header: "Not Authenticated",
-            cssClass: "slds-modal_large, slds-backdrop",
-            body: "Please Authenticate this Customer to continue",
-            showCloseButton: true, 
-            closeCallback: function() {
-                workspaceAPI.closeTab(tabId);        
-            }
-        });
-    },
 
-    handleShowCustomModal: function(component, evt, helper) {
-        if(component.get("v.isActive")) {
-            var workspaceAPI = component.find("workspace");
-            var tabId = workspaceAPI.getEnclosingTabId();
-            var modalBody;
-            $A.createComponent("c:newman", {},
-                            function(content, status) {
-                                if (status === "SUCCESS") {
-                                    modalBody = content;
-                                    component.find('overlayLib').showCustomModal({
-                                        header: "Not Authenticated",
-                                        body: modalBody, 
-                                        showCloseButton: true,
-                                        closeCallback: function() {
-                                                workspaceAPI.closeTab(tabId);        
-                                            }   
-                                    })   
+        var workspaceAPI = component.find("workspace");
+        var tabId = workspaceAPI.getEnclosingTabId();
+        var modalBody;
+        $A.createComponent("c:newman", {},
+            function(content, status) {
+                if (status === "SUCCESS") {
+                    modalBody = content;
+                    component.find('overlayLib').showCustomModal({
+                        header: "Not Authenticated",
+                        body: modalBody, 
+                        showCloseButton: true,
+                        closeCallback: function() {
+                            workspaceAPI.closeTab(tabId);
+                            workspaceAPI.openTab({
+                                pageReference: pageReference,
+                                focus: true
+                            }).then(function(response){
+                                 workspaceAPI.focusTab({tabId : response}); 
                                 }
-                            });
-        }
+                            );
+                            
+                        }   
+                    })   
+                }
+            });
     }
 })
